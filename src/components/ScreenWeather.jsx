7839081ebee881a513ebdc54650bc07aca80a097
Rai-vaz <ray.vaz04@gmail.com>
2023-02-16 22:33:35 -0300

    implementação da descrição

--- src/components/ScreenWeather.jsx
+++ src/components/ScreenWeather.jsx
@@ -7,29 +7,16 @@ import FlagCountry from './FlagCountry'
 
 const ScreenWeather = () => {
     const urlWeatherCall = 'https://api.openweathermap.org/data/2.5/weather?'
-    const urlGeoCall = 'http://api.openweathermap.org/geo/1.0/direct?'
     const apiKey = '2c23509ba3aa57c4c8a36d1aa4a6b1b4';
 
     const [city, setCity] = useState('')
     const [dataWeather, setDataWeather] = useState({})
 
-   async function searchCity() {
-       const res = await fetch(`${urlGeoCall}q=${city}&limit=1&appid=${apiKey}`);
-        const data = await res.json()
-        data.forEach(element => {
-            var {lat, lon} = element
-            seachWeather(lat, lon)
 
-        });  
-    }
-
-    async function seachWeather(lat, lon) {
-       const res = await fetch(`${urlWeatherCall}lat=${lat}&lon=${lon}&appid=${apiKey}`)
+    async function searchWeather() {
+       const res = await fetch(`${urlWeatherCall}q=${city}&lang=pt_br&appid=${apiKey}`)
        const data = await res.json()
-       //console.log(data)
-       setDataWeather(data)
-
-        
+       setDataWeather(data) 
     }
 
   return (
@@ -43,7 +30,7 @@ const ScreenWeather = () => {
                     id='city-input'
                     onChange={(e) => setCity(e.target.value)}
                 />
-                <button id="search" onClick={searchCity}> <FaSearch/> </button>
+                <button id="search" onClick={searchWeather}> <FaSearch/> </button>
             </div>
         </div>
         <div id='weather-data'>
@@ -54,10 +41,11 @@ const ScreenWeather = () => {
                     Object.keys(dataWeather).length > 0 && <FlagCountry src={`https://flagsapi.com/${dataWeather.sys.country}/shiny/64.png`} alt='Bandeira do país'/>
                }
             </h2>
-            <p id='temperature'><span>38 °C</span></p>
+            <p id='temperature'><span>{dataWeather && parseInt(dataWeather.main?.temp) } °C</span></p>
             <div id='description-container'>
-                <p id='description'>Nublado</p>
-                <img src='http://openweathermap.org/img/wn/01d.png' alt='Condições do tempo' id='weather-icon'/>
+                <p id='description'>{Object.keys(dataWeather).length > 0 && dataWeather.weather[0].description}</p>
+                { Object.keys(dataWeather).length > 0 && <FlagCountry src={`http://openweathermap.org/img/wn/${dataWeather.weather[0].icon}.png`} alt='Condições do tempo' id='weather-icon'/> }
+                
             </div>
         </div>
         <div id='details-container'>
